43a901a0d407116b8ddcc740695f9689b53b7782
Naseer ahmad <90660426+NaseerAhmad786@users.noreply.github.com>
2023-07-16 17:39:02 +0500

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const apiKey = 'please your api here';
+export const apiKey = 'place your api here';
 // in some cases your api key maybe already expired
 // try to use a new account to create an api key
 
